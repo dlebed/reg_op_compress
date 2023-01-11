cca78241ff6c49b2af855c6ddb8cd2708f736033
Dmitrii Lebed <lebed.dmitry@gmail.com>
2023-01-10 21:48:59 -0800

    Add PoC of compile-time structure compression

--- include/reg_op_compress.hpp
+++ include/reg_op_compress.hpp
@@ -11,8 +11,66 @@
 
 namespace hal {
 
+struct reg_op_entry {
+    uint16_t    offset;
+    uint32_t    value;
+};
 
+consteval size_t reg_op_comp_size(const struct reg_op_entry entries[])
+{
+    size_t size = 1;
+    size_t i = 1;
 
+    while (entries[i].offset != 0xFFFF) {
+        if (entries[i - 1].value != entries[i].value) {
+            size++;
+        }
+
+        i++;
+    }
+
+    return size;
+}
+
+struct reg_op_compressed_entry {
+    uint16_t    offset;
+    uint16_t    repeat_count;
+    uint32_t    value;
+};
+
+template <const struct reg_op_entry orig[]>
+struct reg_op_compressed {
+    static constexpr size_t element_count = reg_op_comp_size(orig);
+
+    struct reg_op_compressed_entry entries[reg_op_comp_size(orig)];
+
+    consteval reg_op_compressed() : entries()
+    {
+        size_t repeat_count = 0;
+        size_t out_idx = 0;
+        size_t i = 1;
+
+        entries[out_idx].offset = orig[0].offset;
+        entries[out_idx].value = orig[0].value;
+
+        while (orig[i].offset != 0xFFFF) {
+            if (orig[i - 1].value == orig[i].value) {
+                repeat_count++;
+            } else {
+                entries[out_idx].repeat_count = static_cast<uint16_t>(repeat_count);
+                out_idx++;
+                repeat_count = 0;
+
+                entries[out_idx].offset = orig[i].offset;
+                entries[out_idx].value = orig[i].value;
+            }
+
+            i++;
+        }
+
+        entries[out_idx].repeat_count = static_cast<uint16_t>(repeat_count);
+    }
+};
 
 
 }
--- tests/test_reg_op_compress.cpp
+++ tests/test_reg_op_compress.cpp
@@ -9,7 +9,31 @@
 
 using namespace hal;
 
+constexpr struct reg_op_entry reg_op[] = {
+    { 0, 123 },
+    { 4, 123 },
+    { 8, 123 },
+    { 12, 2 },
+    { 16, 2 },
+
+    {0xFFFF}
+};
+
+constexpr reg_op_compressed comp = reg_op_compressed<reg_op>();
+
 TEST(BitFieldSetTest, BasicTest)
 {
-	EXPECT_EQ(2 * 2, 4);
+    EXPECT_EQ(reg_op_comp_size(reg_op), 2);
+
+    EXPECT_EQ(comp.element_count, 2);
+
+    EXPECT_EQ(comp.entries[0].offset, 0);
+    EXPECT_EQ(comp.entries[0].repeat_count, 2);
+    EXPECT_EQ(comp.entries[0].value, 123);
+
+    EXPECT_EQ(comp.entries[1].offset, 12);
+    EXPECT_EQ(comp.entries[1].repeat_count, 1);
+    EXPECT_EQ(comp.entries[1].value, 2);
+
+    EXPECT_EQ(sizeof(comp), 8 * 2);
 }
